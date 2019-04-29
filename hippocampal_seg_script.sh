46b6e9294be5a9573aafb20c804c8ca0b9fb4f87
johyrao <jxrao@ucsd.edu>
2019-04-29 10:26:18 -0700

    Changed from interactive script to final

--- hippocampal_seg_script.sh
+++ hippocampal_seg_script.sh
@@ -1,41 +1,47 @@
 #!/bin/bash
 
-#Johnny Rao 04.08.19
+#Johnny Rao 04.29.19
 #bash script to run freesurfer hippocampal segementation on T1 with T2 scan
 
 export SUBJECTS_DIR="../data"
 
-echo "\nSpecify prefix of T2 scan for subjects: "
-read T2Scan
-echo "\nSpecify analysis ID: "
-read ID
-echo "\nSpecify number of threads to use: "
-read Threads
-echo "\n"
+if [ -z "$1" ]; then
 
-failCount=0
-
-for subject in "../data/" ; do
-
-	#Running recon-all subject by subject in the directory
-	"recon-all -s $subject -hippocampal-subfields-T1T2 $T2Scan*.nii $ID \
-		-itkthreads $Threads"
-
-	#Keeping track of how many failed runs
-	if [ $? -ne 0 ]; then
-
-		failCount=$failCount + 1
-		echo "Subject: $subject failed \n" >> ../data/log.txt
-
-	fi
-
-done
-
-#Print out the number of failed runs and a list of the subject that failed
-echo "$failCount subject(s) failed"
-if [ $failCount != 0 ]; then
-
-	echo "Here are the subject(s) that failed: \n"
-	cat "../data/log.txt"
-
+	echo " ./hippocamp_seg_script <T2 file> <SUbject ID> <Optional Threads count>\n"
+	exit 0
+	
 fi
+
+subject=$1
+
+if [ -z "$2" ]; then
+	
+	echo " ./hippocamp_seg_script <T2 file> <SUbject ID> <Optional Threads count>\n"
+	exit 0
+	
+fi
+
+ID=$2
+
+if [ -z "$3" ]; then
+	
+	echo " Threads count will be set to 4 (default)\n"
+	Threads=4
+	
+fi
+
+Threads=$3
+
+if [ -n "$4" ]; then
+	
+	echo " ./hippocamp_seg_script <T2 file> <SUbject ID> <Optional Threads count>\n"
+	exit 0
+	
+fi
+
+	
+
+#Running recon-all subject by subject in the directory
+"recon-all -s $subject -hippocampal-subfields-T1T2 $T2Scan*.nii $ID \
+	-itkthreads $Threads"
+	
